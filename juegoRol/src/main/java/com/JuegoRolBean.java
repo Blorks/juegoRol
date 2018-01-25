f0c525db763200b6b32fbb7d38fe58021e0cb0b6
Dani <blorkszb@gmail.com>
2018-01-25 23:09:17 +0100

    asd

---
+++ juegoRol/src/main/java/com/JuegoRolBean.java
@@ -0,0 +1,18 @@
+package com;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class JuegoRolBean implements Serializable {
+
+	/*
+	 * Attributes
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
